67f834270519644b2aec662a61cd1c4a3070ffda
GOTOH Shunsuke <antimon2.me@gmail.com>
2020-09-08 22:02:16 +0900

    Adjust Narrow-Bitrange Integers

--- src/Squareness.jl
+++ src/Squareness.jl
@@ -6,18 +6,22 @@ _num_iteration(::I) where (I <: Integer) = 10 - leading_zeros(sizeof(I) % UInt8 
 _num_iteration(::Bool) = 0
 _num_iteration(x::BigInt) = 66 - leading_zeros(sizeof(x.d) * UInt64(abs(x.size)) - 0x01)
 
+_inttype(::Type{UInt}) = UInt
+_inttype(::Type{I}) where {I<:Integer} = promote_type(I, UInt)
+
 function issquare_with_padic_newton(x::I) where {I <: Integer}
     x == 0 && return true
     xtz = trailing_zeros(x)
     isodd(xtz) && return false
     x >>= unsigned(xtz)
-    y::I = 1
+    _I = _inttype(I)
+    y::_I = 1
     N = _num_iteration(x)
     for _ in 1:N
         y = ((I(3) - y * y * x) * y) >> 0x1
     end
-    xrt = x * y
-    m = one(I) << (UInt(1) << N)
+    xrt::_I = x * y
+    m::_I = one(_I) << (UInt(1) << N)
     if xrt & m != 0
         xrt = -xrt
     end
